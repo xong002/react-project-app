14916e3615cda66da40562b5a96a5499c1ea837b
xong002 <xong002@gmail.com>
2023-01-26 16:32:36 +0800

    removed key from Object.entries

--- src/components/funcGetLocDetails.js
+++ src/components/funcGetLocDetails.js
@@ -1,7 +1,7 @@
 import OneMapAPI from "../api/OneMapAPI";
 
-function funcGetLocDetails([lat,long], handler){
-    
+function funcGetLocDetails([lat, long], handler) {
+
     const searchParams = {
         latitude: lat,
         longitude: long,
@@ -18,12 +18,12 @@ function funcGetLocDetails([lat,long], handler){
         try {
             const response = await OneMapAPI.get(`/privateapi/commonsvc/revgeocode?location=${searchParams.latitude},${searchParams.longitude}&token=${searchParams.token}&buffer=${searchParams.buffer}&addressType=${searchParams.addressType}`)
 
-            const convertResults = {...response.data.GeocodeInfo}
-            console.log(convertResults);     
+            const convertResults = { ...response.data.GeocodeInfo }
+            console.log(convertResults);
 
 
-            for (const [key, details] of Object.entries(convertResults)) {
-                
+            for (const [details] of Object.entries(convertResults)) {
+
                 let result = {
                     ADDRESS: null,
                     BLOCK: null,
@@ -33,32 +33,32 @@ function funcGetLocDetails([lat,long], handler){
                     LATITUDE: null,
                     LONGITUDE: null
                 }
-                for (const[key,value] of Object.entries(details)){
+                for (const [key, value] of Object.entries(details)) {
                     result[key] = value
                     console.log(`${key}: ${value}`);
                 }
 
                 if (result.ADDRESS === null) {
-                    
+
                     let address;
 
                     if (result.BLOCK !== null && result.BLOCK !== "null" && result.BLOCK !== "NIL") address = result.BLOCK
-                    if (result.ROAD !== null && result.ROAD !== "null" && result.ROAD !== "NIL") address = address +" "+ result.ROAD
-                    if (result.BUILDINGNAME !== null && result.BUILDINGNAME !== "null"&& result.BUILDINGNAME !== "NIL") address = address +" "+ result.BUILDING
-                    if (result.POSTALCODE !== null && result.POSTALCODE !== "null" && result.POSTALCODE !== "NIL"&& result.POSTALCODE !== "") address = address + " SINGAPORE " + result.POSTALCODE
-                    
+                    if (result.ROAD !== null && result.ROAD !== "null" && result.ROAD !== "NIL") address = address + " " + result.ROAD
+                    if (result.BUILDINGNAME !== null && result.BUILDINGNAME !== "null" && result.BUILDINGNAME !== "NIL") address = address + " " + result.BUILDING
+                    if (result.POSTALCODE !== null && result.POSTALCODE !== "null" && result.POSTALCODE !== "NIL" && result.POSTALCODE !== "") address = address + " SINGAPORE " + result.POSTALCODE
+
                     address = address.replace("undefined", "")
                     address = address.replace("null", "")
 
                     result.ADDRESS = address;
                 }
 
-                resultList= [...resultList,result]
-              }
+                resultList = [...resultList, result]
+            }
 
             console.log(resultList);
-            handler(0,resultList[0]);
-        
+            handler(0, resultList[0]);
+
         } catch (error) {
             console.log(error.message);
         }
