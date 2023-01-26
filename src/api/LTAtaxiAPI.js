5c85a8e042f0dc9269b9a5f68b3dfc9c0cb39a72
xong002 <xong002@gmail.com>
2023-01-26 22:22:47 +0800

    change LTAtaxiAPI without proxy

--- src/api/LTAtaxiAPI.js
+++ src/api/LTAtaxiAPI.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const BASE_URL = "http://datamall2.mytransport.sg/ltaodataservice";
-const CROSS_DOMAIN = "https://cors-anywhere.herokuapp.com";
-const REQUEST_URL = `${CROSS_DOMAIN}/${BASE_URL}`;
+// const CROSS_DOMAIN = "https://cors-anywhere.herokuapp.com";
+// const REQUEST_URL = `${CROSS_DOMAIN}/${BASE_URL}`;
 
 const LTAtaxiAPI = axios.create({
-    baseURL: REQUEST_URL,
-    // baseURL: BASE_URL,
+    // baseURL: REQUEST_URL,
+    baseURL: BASE_URL,
     headers: {
         AccountKey: `${process.env.REACT_APP_LTA_API_KEY}`,
         accept: "application/json",
